a492cf87617a6ad4d6125c7ae5e10fe0c8eaec2c
Sangmin <sm372181@gmail.com>
2017-12-06 19:27:31 +0900

    [ADJUST] HelloWorld

--- 001_HelloWorld!/Hello.cs
+++ 001_HelloWorld!/Hello.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-namespace HelloWorld
+class Hello
 {
-    class Hello
+    static void Main()
     {
-        static void Main()
-        {
-            //C#에서 출력은 Console.WriteLine을 사용한다. (JAVA의 System.out.println같은 출력)
-            Console.WriteLine("Hello World!");
+        //C#에서 출력은 Console.WriteLine을 사용한다. (JAVA의 System.out.println같은 출력)
+        Console.WriteLine("Hello World!");
 
-            //Console.Write로도 출력을 할 수 있다. (JAVA의 System.out.print같은 출력)
-            Console.WriteLine("Press any key to exit.");
-            //ReadKey는 키를 입력받는 함수이다. 키가 입력되기 전까지 프로그램을 정지시켜 프로그램의 종료를 막는다.
-            Console.ReadKey();
-        }
+        //Console.Write로도 출력을 할 수 있다. (JAVA의 System.out.print같은 출력)
+        Console.WriteLine("Press any key to exit.");
+        //ReadKey는 키를 입력받는 함수이다. 키가 입력되기 전까지 프로그램을 정지시켜 프로그램의 종료를 막는다.
+        Console.ReadKey();
     }
 }
