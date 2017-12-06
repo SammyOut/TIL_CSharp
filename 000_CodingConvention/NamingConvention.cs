275234e3f45b00b2912d0d0977dcef40ac97eb86
Sangmin <sm372181@gmail.com>
2017-12-06 19:26:15 +0900

    [ADD] NamingConvention

---
+++ 000_CodingConvention/NamingConvention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// 클래스 이름은 PscalCase를 사용한다.
+// 파일의 이름과 클래스의 이름은 같아야 한다.
+public class NamingConvention
+{
+    // 메소드 이름에도 PascalCase를 사용한다.
+    static void Main()
+    {
+        // 변수 이름은 camelCase를 사용한다.
+        int goodVar = 10;
+        //boolean 변수, 속성, 함수에는 is 또는 유사한 접두어(has, can, should)를 붙인 이름을 사용한다.
+        bool isGoodVar = true;
+    }
+}
